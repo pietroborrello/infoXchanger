1ee0e3f6c86a21a9a1381da4f671e3533a4fcd18
pietroborrello <pietro.borrello95@gmail.com>
2017-05-22 16:23:27 +0200

    merged profile and facebook

--- infoXchanger/config/routes.rb
+++ infoXchanger/config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'users/myprofile'
-  devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords' }
+  devise_for :users, controllers: {:omniauth_callbacks => "users/omniauth_callbacks", sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords' }
   resources :users, only: [:show]
-   devise_for :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  #devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
 end
