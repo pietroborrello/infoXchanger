34885e10e9b01256b8a167da4da4eef4ccb1d6da
pietroborrello <pietro.borrello95@gmail.com>
2017-05-27 01:04:41 +0200

    bug on model

--- infoXchanger/app/models/user.rb
+++ infoXchanger/app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook],
-         :omniauth_providers => [:google_oauth2]
+         :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
 
 
    def self.from_omniauth(auth)
