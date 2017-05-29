8ff085ca7922d0250e618f94333c7eea76678160
pietroborrello <pietro.borrello95@gmail.com>
2017-05-30 00:07:44 +0200

    redirection ok

--- infoXchanger/app/controllers/application_controller.rb
+++ infoXchanger/app/controllers/application_controller.rb
@@ -13,14 +13,7 @@ class ApplicationController < ActionController::Base
    end
 
    def after_sign_in_path_for(resource)
-     sign_in_url = new_user_session_url
-     sign_up_url = new_user_registration_url
-     if request.referer == sign_in_url || request.referer == sign_up_url
-       root_path
-       #super
-     else
-       root_path || request.env['omniauth.origin'] || stored_location_for(resource) || request.referer
-     end
+       stored_location_for(resource) || root_path
    end
 
 end
