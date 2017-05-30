5948e1fc243a36c1b4736c32aa4de93fd517684a
Mascoma Federico <mascomafederico@gmail.com>
2017-05-30 14:58:19 +0200

    bug corrected

--- infoXchanger/app/controllers/users/omniauth_callbacks_controller.rb
+++ infoXchanger/app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,6 +12,7 @@
   end
 
   def google_oauth2
+        byebug
         # You need to implement the method below in your model (e.g. app/models/user.rb)
         @user = User.from_omniauth(request.env['omniauth.auth'])
 
--- infoXchanger/app/models/user.rb
+++ infoXchanger/app/models/user.rb
@@ -12,9 +12,9 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      #name = auth.info.name
-      #user.first_name, user.last_name = name.split(' ', 2)
-      #user.image_url = auth.info.image
+      name = auth.info.name
+      user.first_name, user.last_name = name.split(' ', 2)
+      user.image_url = auth.info.image
     end
 	end
   def self.new_with_session(params, session)
