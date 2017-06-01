f4695898ac5b14d8467338da37adecae5dfe7546
pietroborrello <pietro.borrello95@gmail.com>
2017-06-01 11:44:34 +0200

    merge select and generate

--- infoXchanger/app/controllers/tokens_controller.rb
+++ infoXchanger/app/controllers/tokens_controller.rb
@@ -1,7 +1,29 @@
 class TokensController < ApplicationController
 
+  @@info =
+  [:address,
+  :born_on,
+  :born_at,
+  :telephone,
+  :website,
+  :social_number,
+  :id_number,
+  :license_number,
+  :car_plate,
+  :insurance_company,
+  :weight,
+  :height,
+  :blood_group]
+
   def create
-    info_str = "0 1 2 3 4 5 6 7 8 9 10 11 12"
+    info_str = ""
+    @@info.each do |info|
+      if params[info] != nil
+        info_str << @@info.index(info).to_i.to_s << " "
+      end
+    end
+    #info_str = "0 1 2 3 4 5 6 7 8 9 10 11 12"
+    byebug
     @user = current_user
     password = ''
     token_hash = SecureRandom.urlsafe_base64(18)
