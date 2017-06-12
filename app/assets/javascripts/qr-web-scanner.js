630e2b58509d965b3e54ae88980150ce20e81aa0
pietroborrello <pietro.borrello95@gmail.com>
2017-06-12 18:18:42 +0200

    bug on scanner

--- app/assets/javascripts/qr-web-scanner.js
+++ app/assets/javascripts/qr-web-scanner.js
@@ -332,16 +332,50 @@ var QRWebScanner = (function () {
             window.URL.createObjectURL = window.URL.createObjectURL || window.URL.webkitCreateObjectURL || window.URL.mozCreateObjectURL || window.URL.msCreateObjectURL;
 
             var options = true;
-            navigator.mediaDevices.getUserMedia({video: options}).then(
-                function (stream) {
-                    Get.videoBox().src = window.URL.createObjectURL(stream);
-                    Canvas.captureImage(Get.videoBox());
+            if(navigator.mediaDevices && navigator.mediaDevices.enumerateDevices)
+            {
+                try{
+                    navigator.mediaDevices.enumerateDevices()
+                    .then(function(devices) {
+                      devices.forEach(function(device) {
+                        if (device.kind === 'videoinput') {
+                          if(device.label.toLowerCase().search("back") >-1)
+                            options={deviceId: {exact: device.deviceId}};
+                        }
+                        console.log(device.kind + ": " + device.label +
+                                    " id = " + device.deviceId);
+                      });
+                      navigator.mediaDevices.getUserMedia({video: options}).then(
+                          function (stream) {
+                              Get.videoBox().src = window.URL.createObjectURL(stream);
+                              Canvas.captureImage(Get.videoBox());
 
-                    WebCam.state = true;
+                              WebCam.state = true;
 
-                }).catch(function (error) {
-                    console.log('With the video stream that something is wrong or the user banned :P');
-                });
+                          }).catch(function (error) {
+                              console.log('With the video stream that something is wrong or the user banned :P');
+                          });
+                    })
+                }
+                catch(e)
+                {
+                    console.log(e);
+                }
+            }
+            else{
+                console.log("no navigator.mediaDevices.enumerateDevices" );
+                navigator.mediaDevices.getUserMedia({video: options}).then(
+                    function (stream) {
+
+                        Get.videoBox().src = window.URL.createObjectURL(stream);
+                        Canvas.captureImage(Get.videoBox());
+
+                        WebCam.state = true;
+
+                    }).catch(function (error) {
+                        console.log('With the video stream that something is wrong or the user banned :P');
+                    });
+            }
         }
 
     },
